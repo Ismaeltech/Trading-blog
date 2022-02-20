745a7af5786077ef640c40a7200e9340379c2a51
Ismael <edy1017@live.com>
2022-02-19 19:46:37 -0500

    added styling to menu icon and mobile nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -40,7 +40,7 @@ export default {
 header {
     background-color: #fff;
     padding: 0 25px;
-    box-shadow: 0 4x 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    box-shadow: 0 2px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     z-index: 99;
 
     .link {
@@ -87,6 +87,33 @@ header {
                    margin-right: 0;
                }
             }
+        }
+    }
+
+    .menu-icon {
+        cursor: pointer;
+        position: absolute;
+        top: 32px;
+        right: 25px;
+        height: 25px;
+        width: auto;
+    }
+
+    .mobile-nav {
+        padding: 20px;
+        width: 70%;
+        max-width: 250px;
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        height: 100%;
+        background-color: #303030;
+        top: 0;
+        left: 0;
+
+        .link {
+            padding: 15px 0;
+            color: #fff;
         }
     }
 }
