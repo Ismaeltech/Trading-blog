84fc827198cfb9e4239d74c974fd2a4cfae1be1f
Ismael <edy1017@live.com>
2022-02-21 03:54:39 -0500

    Added data sampleBlogCards, an array that has a blogtitle, blogcoverphoto and the blogdate in home.vue. Imported and registered BlogCards component. Added blogcards html in home.vue.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <BlogPost :post="welcomeScreen" />
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost";
+import BlogCards from "../components/BlogCard";
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCards },
   data() {
     return {
       welcomeScreen: {
@@ -30,6 +39,12 @@ export default {
           blogHTML: "This is a filler blog post title",
           blogCoverPhoto: "trading",
         },
+      ],
+      sampleBlogCards: [
+        { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "February 21, 2022 " },
+        { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "February 21, 2022 " },
+        { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "February 21, 2022 " },
+        { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "February 21, 2022 " },
       ],
     };
   },
